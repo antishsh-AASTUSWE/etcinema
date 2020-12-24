46554d7cdd5d54f35e9e274f4be1514ccf8f6439
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2020-12-24 11:38:39 +0300

    comit for sql  update.

--- database.sql
+++ database.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 07, 2020 at 09:49 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost:8889
+-- Generation Time: Dec 24, 2020 at 08:37 AM
+-- Server version: 5.7.30
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cinemaet`
@@ -50,7 +44,9 @@ CREATE TABLE `cinema` (
 
 INSERT INTO `cinema` (`cinema_id`, `cinema_name`) VALUES
 (1, 'Edna Mall'),
-(4, 'Century');
+(5, 'Alemu'),
+(6, 'Ethiopi'),
+(8, 'Agona');
 
 -- --------------------------------------------------------
 
@@ -70,7 +66,7 @@ CREATE TABLE `geners` (
 INSERT INTO `geners` (`gener_id`, `gener`) VALUES
 (2, 'Drama'),
 (3, 'Romance'),
-(4, 'Crime');
+(7, 'Action');
 
 -- --------------------------------------------------------
 
@@ -93,6 +89,13 @@ CREATE TABLE `movie` (
   `mov_subtitle` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`movie_id`, `mov_name`, `mov_plot`, `mov_gener`, `mov_ratting`, `mov_poster`, `mov_trailor`, `mov_running_time`, `mov_realse_date`, `mov_starring`, `mov_language`, `mov_subtitle`) VALUES
+(2, 'test', 'test', 2, 2, 'boss_baby_family_business_xlg.jpg', 'test', 'test', '0000-00-00', 'test', 'English', 'English');
+
 -- --------------------------------------------------------
 
 --
@@ -100,7 +103,7 @@ CREATE TABLE `movie` (
 --
 
 CREATE TABLE `ratings` (
-  `id` int(11) NOT NULL,
+  `rating_id` int(11) NOT NULL,
   `rating` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -109,7 +112,7 @@ CREATE TABLE `ratings` (
 -- Dumping data for table `ratings`
 --
 
-INSERT INTO `ratings` (`id`, `rating`, `description`) VALUES
+INSERT INTO `ratings` (`rating_id`, `rating`, `description`) VALUES
 (1, 'G', 'General '),
 (2, 'PG', 'Parental '),
 (3, 'PG-13', 'Parents Strongly Cautioned – Some material may be inappropriate for children under 13.\r\n'),
@@ -152,17 +155,23 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `phone_no` varchar(15) NOT NULL,
-  `date_of_birth` date NOT NULL,
-  `password` varchar(255) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `name`, `email`, `phone_no`, `date_of_birth`, `password`) VALUES
-(2, 'yeshewas', 'yeshewasg13@gmail.com', '2147483647', '2020-09-10', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` (`user_id`, `name`, `email`, `username`, `password`, `role`, `status`, `created_at`) VALUES
+(2, 'yeshewas', '', '', '202cb962ac59075b964b07152d234b70', '', '0', '2020-12-22 00:21:26'),
+(3, 'anteneh', 'anteneh@gmail.com', 'antish', '1234', 'admin', 'active', '2020-12-22 00:35:34'),
+(5, 'anteneh', 'anteneh@gmail.com', 'antish', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', 'active', '2020-12-22 00:41:56'),
+(6, 'yalfal', 'yalfal@gmail.com', 'yalfal', '962012d09b8170d912f0669f6d7d9d07', 'manager', 'active', '2020-12-24 07:55:24'),
+(7, 'barnabas', 'barni@gmail.com', 'barni', '81dc9bdb52d04dc20036dbd8313ed055', 'staff', 'active', '2020-12-24 07:56:02');
 
 --
 -- Indexes for dumped tables
@@ -204,7 +213,7 @@ ALTER TABLE `movie`
 -- Indexes for table `ratings`
 --
 ALTER TABLE `ratings`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`rating_id`);
 
 --
 -- Indexes for table `seat`
@@ -235,41 +244,49 @@ ALTER TABLE `user`
 --
 ALTER TABLE `bookinf_info`
   MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `cinema`
 --
 ALTER TABLE `cinema`
-  MODIFY `cinema_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `cinema_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `geners`
 --
 ALTER TABLE `geners`
-  MODIFY `gener_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `gener_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `ratings`
 --
 ALTER TABLE `ratings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `seat`
 --
 ALTER TABLE `seat`
   MODIFY `seat_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `showtime`
 --
 ALTER TABLE `showtime`
   MODIFY `show_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- Constraints for dumped tables
 --
@@ -286,7 +303,7 @@ ALTER TABLE `bookinf_info`
 -- Constraints for table `movie`
 --
 ALTER TABLE `movie`
-  ADD CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`mov_ratting`) REFERENCES `ratings` (`id`),
+  ADD CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`mov_ratting`) REFERENCES `ratings` (`rating_id`),
   ADD CONSTRAINT `movie_ibfk_2` FOREIGN KEY (`mov_gener`) REFERENCES `geners` (`gener_id`);
 
 --
@@ -296,7 +313,3 @@ ALTER TABLE `showtime`
   ADD CONSTRAINT `showtime_ibfk_1` FOREIGN KEY (`cinema_id`) REFERENCES `cinema` (`cinema_id`),
   ADD CONSTRAINT `showtime_ibfk_2` FOREIGN KEY (`cinema_id`) REFERENCES `cinema` (`cinema_id`),
   ADD CONSTRAINT `showtime_ibfk_3` FOREIGN KEY (`mov_id`) REFERENCES `movie` (`movie_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
