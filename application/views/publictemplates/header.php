946a84b0ff6491a2271921ebf6b21ffe8d88ba6e
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2020-12-27 05:04:07 +0300

    last

--- application/views/publictemplates/header.php
+++ application/views/publictemplates/header.php
@@ -31,14 +31,14 @@
 <body>
 
     <!-- ==========Preloader========== -->
-    <div class="preloader">
+    <!-- <div class="preloader">
         <div class="preloader-inner">
             <div class="preloader-icon">
                 <span></span>
                 <span></span>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- ==========Preloader========== -->
     <!-- ==========Overlay========== -->
     <div class="overlay"></div>
